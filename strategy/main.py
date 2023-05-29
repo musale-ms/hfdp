3d75a8b19417427e925b202010967731ec5e4919
Martin Musale <martinmusale@microsoft.com>
2023-05-29 16:48:32 +0100

    Py: Show inheritance from an abstract class

--- strategy/main.py
+++ strategy/main.py
@@ -1,7 +1,29 @@
-class Duck:
-    pass
+from abc import ABC, abstractmethod
+
+class Duck(ABC):
+    @abstractmethod
+    def quack(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def swim(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def display(self):
+        raise NotImplementedError
+
+class MallardDuck(Duck):
+    def quack(self):
+        print("Mallard duck quack")
+
+    def swim(self):
+        print("Mallard duck swim")
+
+    def display(self):
+        print("Mallard duck hello")
 
 
 if __name__ == '__main__':
-    duck = Duck()
-    print(duck)
+    mallard = MallardDuck()
+    print(mallard.swim())
